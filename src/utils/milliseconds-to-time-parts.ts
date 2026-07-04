1df2c4658a42270db227bdde666e21c13679ed31
Elson Correia <elcorreia45@gmail.com>
2026-07-04 17:34:13 -0400

    fix: align duration time part keys

--- src/utils/milliseconds-to-time-parts.ts
+++ src/utils/milliseconds-to-time-parts.ts
@@ -17,58 +17,58 @@ import {
 export const millisecondsToTimeParts = (mils: number, keys: Set<string>) => {
     let remainder = mils
     const obj = {
-        year: 0,
-        month: 0,
-        week: 0,
-        day: 0,
-        hour: 0,
-        minute: 0,
-        second: 0,
-        millisecond: 0,
-        nanosecond: 0,
+        years: 0,
+        months: 0,
+        weeks: 0,
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+        milliseconds: 0,
+        nanoseconds: 0,
     }
 
-    if (keys.has('year')) {
-        obj.year = Math.floor(mils / ONE_YEAR_MS)
+    if (keys.has('years')) {
+        obj.years = Math.floor(mils / ONE_YEAR_MS)
         remainder = mils % ONE_YEAR_MS
     }
 
-    if (keys.has('month')) {
-        obj.month = Math.floor(remainder / ONE_MONTH_MS)
+    if (keys.has('months')) {
+        obj.months = Math.floor(remainder / ONE_MONTH_MS)
         remainder = remainder % ONE_MONTH_MS
     }
 
-    if (keys.has('week')) {
-        obj.week = Math.floor(remainder / ONE_WEEK_MS)
+    if (keys.has('weeks')) {
+        obj.weeks = Math.floor(remainder / ONE_WEEK_MS)
         remainder = remainder % ONE_WEEK_MS
     }
 
-    if (keys.has('day')) {
-        obj.day = Math.floor(remainder / ONE_DAY_MS)
+    if (keys.has('days')) {
+        obj.days = Math.floor(remainder / ONE_DAY_MS)
         remainder = remainder % ONE_DAY_MS
     }
 
-    if (keys.has('hour')) {
-        obj.hour = Math.floor(remainder / ONE_HOUR_MS)
+    if (keys.has('hours')) {
+        obj.hours = Math.floor(remainder / ONE_HOUR_MS)
         remainder = remainder % ONE_HOUR_MS
     }
 
-    if (keys.has('minute')) {
-        obj.minute = Math.floor(remainder / ONE_MINUTE_MS)
+    if (keys.has('minutes')) {
+        obj.minutes = Math.floor(remainder / ONE_MINUTE_MS)
         remainder = remainder % ONE_MINUTE_MS
     }
 
-    if (keys.has('second')) {
-        obj.second = Math.floor(remainder / ONE_SECOND_MS)
+    if (keys.has('seconds')) {
+        obj.seconds = Math.floor(remainder / ONE_SECOND_MS)
         remainder = remainder % ONE_SECOND_MS
     }
 
-    if (keys.has('millisecond')) {
-        obj.millisecond = remainder
+    if (keys.has('milliseconds')) {
+        obj.milliseconds = remainder
     }
 
-    if (keys.has('nanosecond')) {
-        obj.nanosecond = remainder * ONE_NANOSECOND_MS
+    if (keys.has('nanoseconds')) {
+        obj.nanoseconds = remainder * ONE_NANOSECOND_MS
     }
 
     return obj
